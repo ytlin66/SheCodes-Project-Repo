6fb9ce23d07a1d02ed8e24da7959c17720904ffc
ytlin66 <ytlin66@gmail.com>
2022-03-17 09:08:45 -0400

    Added search function

--- script.js
+++ script.js
@@ -13,26 +13,69 @@ let day = daysOfWeek[today.getDay()];
 let time = today.getHours() + ":" + today.getMinutes();
 let todayDate = document.querySelector("#date");
 todayDate.innerHTML = `${day} ${time}`;
-//change city to whatever you entered
+//below is to change city to whatever you entered
 function changeCity(event) {
   event.preventDefault();
   let cityText = document.querySelector("#city");
   let enteredCity = document.querySelector("#city-input");
   cityText.innerHTML = enteredCity.value;
+  //below is to get weather API and URL.
+  let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+  let apiKey = "168a6f3c90ccb5d34accdfb30077f21f";
+  let city = enteredCity.value;
+  let units = "metric";
+  let apiUrl = `${apiEndpoint}?q=${city}&units=${units}&appid=${apiKey}`;
+  axios.get(apiUrl).then(changeTemp);
+  axios.get(apiUrl).then(changeDescription);
+  axios.get(apiUrl).then(changeWind);
+  axios.get(apiUrl).then(changeHumidity);
+  axios.get(apiUrl).then(changePrecipitation);
+}
+
+function changeTemp(response) {
+  console.log(response);
+  let newTemperature = response.data.main.temp;
+  let displayTemp = document.querySelector("#temperature");
+  displayTemp.innerHTML = newTemperature;
+}
+function changeDescription(response) {
+  let description = response.data.weather[0].description;
+  let selectDescription = document.querySelector("#description");
+  selectDescription.innerHTML = description;
+}
+function changeWind(response) {
+  let newWind = response.data.wind.speed;
+  let selectWind = document.querySelector("#wind");
+  selectWind.innerHTML = `Wind: ${newWind} km/h`;
+}
+function changeHumidity(response) {
+  let newHumid = response.data.main.humidity;
+  let selectHumid = document.querySelector("#humidity");
+  selectHumid.innerHTML = `Humidity: ${newHumid}%`;
+}
+function changePrecipitation(response) {
+  let newPreci = response.data.main.precipitation;
+  let selectPreci = document.querySelector("#precipitation");
+  selectPreci.innerHTML = `Precipitation: ${newPreci}%`;
+}
+function getLatAndLon(position) {
+  console.log(position.coords.latitude);
+  let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+  let apiKey = "168a6f3c90ccb5d34accdfb30077f21f";
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  let units = "metric";
+  let apiUrl = `${apiEndpoint}?lat=${lat}&lon=${lon}&units=${units}&appid=${apiKey}`;
+  axios.get(apiUrl).then(changeTemp);
+  axios.get(apiUrl).then(changeDescription);
+  axios.get(apiUrl).then(changeWind);
+  axios.get(apiUrl).then(changeHumidity);
+  axios.get(apiUrl).then(changePrecipitation);
+}
+function getCurrentLocation() {
+  navigator.geolocation.getCurrentPosition(getLatAndLon);
 }
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", changeCity);
-//change temperature when clicking on C or F.
-function changeTempC() {
-  let temp = document.querySelector("#temperature");
-  temp.innerHTML = "66";
-}
-function changeTempF() {
-  let temp = document.querySelector("#temperature");
-  temp.innerHTML = "19";
-}
-let c = document.querySelector("#celsius-link");
-c.addEventListener("click", changeTempC);
-
-let f = document.querySelector("#fahrenheit-link");
-f.addEventListener("click", changeTempF);
+let currentButton = document.querySelector("#current-location-button");
+currentButton.addEventListener("click", getCurrentLocation);
