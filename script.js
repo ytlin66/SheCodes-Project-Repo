8781f0cb336e0fac1375a26ba887aa9472931ec8
ytlin66 <ytlin66@gmail.com>
2022-03-24 09:11:08 -0400

    Refined JS structure, shorter

--- script.js
+++ script.js
@@ -1,3 +1,47 @@
+//below is to change city to whatever you entered
+function changeCity(event) {
+  event.preventDefault();
+  let cityText = document.querySelector("#city");
+  let enteredCity = document.querySelector("#city-input");
+  cityText.innerHTML = enteredCity.value;
+  //below is to get weather API and URL.
+  let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+  let apiKey = "168a6f3c90ccb5d34accdfb30077f21f";
+  let city = enteredCity.value;
+  let units = "metric";
+  let apiUrl = `${apiEndpoint}?q=${city}&units=${units}&appid=${apiKey}`;
+  axios.get(apiUrl).then(changeTempDescripWindHumid);
+}
+
+function changeTempDescripWindHumid(response) {
+  console.log(response);
+  let displayTemp = document.querySelector("#temperature");
+  displayTemp.innerHTML = Math.round(response.data.main.temp);
+  let selectDescription = document.querySelector("#description");
+  selectDescription.innerHTML = response.data.weather[0].description;
+  let selectWind = document.querySelector("#wind");
+  selectWind.innerHTML = `Wind: ${response.data.wind.speed} km/h`;
+  let selectHumid = document.querySelector("#humidity");
+  selectHumid.innerHTML = `Humidity: ${response.data.main.humidity}%`;
+}
+
+//geting the current location
+function getCurrentLocation() {
+  navigator.geolocation.getCurrentPosition(getLatAndLon);
+}
+function getLatAndLon(position) {
+  console.log(position.coords.latitude);
+  let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+  let apiKey = "168a6f3c90ccb5d34accdfb30077f21f";
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  let units = "metric";
+  let apiUrl = `${apiEndpoint}?lat=${lat}&lon=${lon}&units=${units}&appid=${apiKey}`;
+  let currentCity = document.querySelector("#city");
+  currentCity.innerHTML = "Current City";
+  axios.get(apiUrl).then(changeTempDescripWindHumid);
+}
+
 //Show Today's day of week and current time
 let today = new Date();
 let daysOfWeek = [
@@ -13,65 +57,9 @@ let day = daysOfWeek[today.getDay()];
 let time = today.getHours() + ":" + today.getMinutes();
 let todayDate = document.querySelector("#date");
 todayDate.innerHTML = `${day} ${time}`;
-//below is to change city to whatever you entered
-function changeCity(event) {
-  event.preventDefault();
-  let cityText = document.querySelector("#city");
-  let enteredCity = document.querySelector("#city-input");
-  cityText.innerHTML = enteredCity.value;
-  //below is to get weather API and URL.
-  let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
-  let apiKey = "168a6f3c90ccb5d34accdfb30077f21f";
-  let city = enteredCity.value;
-  let units = "metric";
-  let apiUrl = `${apiEndpoint}?q=${city}&units=${units}&appid=${apiKey}`;
-  axios.get(apiUrl).then(changeTemp);
-  axios.get(apiUrl).then(changeDescription);
-  axios.get(apiUrl).then(changeWind);
-  axios.get(apiUrl).then(changeHumidity);
-}
-
-function changeTemp(response) {
-  console.log(response);
-  let newTemperature = response.data.main.temp;
-  let displayTemp = document.querySelector("#temperature");
-  displayTemp.innerHTML = newTemperature;
-}
-function changeDescription(response) {
-  let description = response.data.weather[0].description;
-  let selectDescription = document.querySelector("#description");
-  selectDescription.innerHTML = description;
-}
-function changeWind(response) {
-  let newWind = response.data.wind.speed;
-  let selectWind = document.querySelector("#wind");
-  selectWind.innerHTML = `Wind: ${newWind} km/h`;
-}
-function changeHumidity(response) {
-  let newHumid = response.data.main.humidity;
-  let selectHumid = document.querySelector("#humidity");
-  selectHumid.innerHTML = `Humidity: ${newHumid}%`;
-}
-
-function getLatAndLon(position) {
-  console.log(position.coords.latitude);
-  let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
-  let apiKey = "168a6f3c90ccb5d34accdfb30077f21f";
-  let lat = position.coords.latitude;
-  let lon = position.coords.longitude;
-  let units = "metric";
-  let apiUrl = `${apiEndpoint}?lat=${lat}&lon=${lon}&units=${units}&appid=${apiKey}`;
-  let currentCity = document.querySelector("#city");
-  currentCity.innerHTML = "Current City";
-  axios.get(apiUrl).then(changeTemp);
-  axios.get(apiUrl).then(changeDescription);
-  axios.get(apiUrl).then(changeWind);
-  axios.get(apiUrl).then(changeHumidity);
-}
-function getCurrentLocation() {
-  navigator.geolocation.getCurrentPosition(getLatAndLon);
-}
+//This is for after entering the city, and hit Search.
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", changeCity);
+//this is for the "Current" button
 let currentButton = document.querySelector("#current-location-button");
 currentButton.addEventListener("click", getCurrentLocation);
